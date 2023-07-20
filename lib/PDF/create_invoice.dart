ae87c985835d09c6ab86e8ab92744da0521a0ef0
kapil0078 <kapilkatrodiya125@gmail.com>
2023-07-20 19:34:08 +0530

    PDF Work Table Done

--- lib/PDF/create_invoice.dart
+++ lib/PDF/create_invoice.dart
@@ -6,6 +6,8 @@ import 'package:printing/printing.dart';
 Future<Uint8List> createInvoice() async {
   final pdf = Document();
 
+  //Font
+
   //logo
   final logo = (await rootBundle.load("assets/images/Mahindra-Logo.png"))
       .buffer
@@ -37,10 +39,29 @@ Future<Uint8List> createInvoice() async {
       "qty": 50,
       "price": 500,
       "total": 25000,
-    },
+    }
   ];
+
+  // keyValue
+
+  Widget keyValue({
+    required String key,
+    required String value,
+    double width = 40,
+  }) =>
+      Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(key),
+          SizedBox(width: width),
+          Text(value),
+        ],
+      );
   // Create PDFWidget
   Widget pdfWidget() => Container(
+        width: double.maxFinite,
         child: Column(
           children: [
             Row(
@@ -69,7 +90,7 @@ Future<Uint8List> createInvoice() async {
               ],
             ),
             Padding(
-              padding: const EdgeInsets.only(top: 20, bottom: 15),
+              padding: const EdgeInsets.only(top: 20, bottom: 35),
               child: Divider(
                 height: 0,
                 thickness: 1.5,
@@ -77,11 +98,35 @@ Future<Uint8List> createInvoice() async {
               ),
             ),
             Table(
+              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+              columnWidths: <int, TableColumnWidth>{
+                0: const FlexColumnWidth(3),
+                1: const FlexColumnWidth(1),
+                2: const FlexColumnWidth(1),
+                3: const FlexColumnWidth(2),
+              },
+              border: TableBorder.symmetric(
+                inside: const BorderSide(
+                  color: PdfColors.grey500,
+                  width: 0.75,
+                ),
+                outside: const BorderSide(
+                  color: PdfColors.black,
+                  width: 1,
+                ),
+              ),
               children: [
                 TableRow(
+                  verticalAlignment: TableCellVerticalAlignment.middle,
+                  decoration: BoxDecoration(
+                    color: PdfColor.fromHex("C0C0C0"),
+                  ),
                   children: tableHeader
                       .map(
-                        (e) => Text(e),
+                        (e) => SizedBox(
+                          height: 25,
+                          child: Center(child: Text(e)),
+                        ),
                       )
                       .toList(),
                 ),
@@ -89,15 +134,60 @@ Future<Uint8List> createInvoice() async {
                     .map(
                       (e) => TableRow(
                         children: [
-                          Text(e['item']),
-                          Text("${e['qty']}"),
-                          Text('${e['price']}'),
-                          Text("${e['total']}"),
+                          SizedBox(
+                            height: 20,
+                            child: Center(child: Text(e['item'])),
+                          ),
+                          SizedBox(
+                            height: 20,
+                            child: Center(child: Text("${e['qty']}")),
+                          ),
+                          SizedBox(
+                            height: 20,
+                            child: Center(child: Text('${e['price']}')),
+                          ),
+                          SizedBox(
+                            height: 20,
+                            child: Center(child: Text("${e['total']}")),
+                          ),
                         ],
                       ),
                     )
                     .toList(),
               ],
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: SizedBox(
+                  width: 140,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      keyValue(
+                        key: "Subtotal",
+                        value: List<int>.from(tableRow.map((e) => e["total"]))
+                            .toList()
+                            .reduce((value, element) => value + element)
+                            .toString(),
+                      ),
+                      keyValue(
+                        key: "Tax(0%)",
+                        value: "0",
+                      ),
+                      Divider(
+                        height: 0,
+                        thickness: 1.2,
+                      ),
+                      keyValue(
+                        key: "Total",
+                        value: List<int>.from(tableRow.map((e) => e["total"]))
+                            .toList()
+                            .reduce((value, element) => value + element)
+                            .toString(),
+                        width: 58,
+                      )
+                    ],
+                  )),
             ),
           ],
         ),
